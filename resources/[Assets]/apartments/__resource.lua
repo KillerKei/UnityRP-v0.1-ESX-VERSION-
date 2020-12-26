3d1d80998bbd83ee2b82b3b3cf86bb739ab8b7f3
MortalGhost291 <mortalghost0161@gmail.com>
2020-12-26 20:04:37 +0000

    Replaced irp-errorlog with urp-errorlog (:

--- resources/[Assets]/apartments/__resource.lua
+++ resources/[Assets]/apartments/__resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 
-client_script "@irp-errorlog/client/cl_errorlog.lua"
+client_script "@urp-errorlog/client/cl_errorlog.lua"
 
 client_script 'apart_client.lua'
 server_script 'apart_server.lua'
